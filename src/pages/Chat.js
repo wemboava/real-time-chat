bd03fca478cb1f3ed0cce0d9873fb68b149a9542
Willian <willian@quicktech.no>
2018-07-22 21:31:12 -0300

    :sparkles: apollo configuration

---
+++ src/pages/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Chat = () => (
+  <View>
+    <Text>
+      teste
+    </Text>
+  </View>
+);
+
+export default Chat;
