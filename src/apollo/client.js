0434f865cac5eaf1dabd39b37d7408b12d59e308
Willian <willian@quicktech.no>
2018-07-29 16:59:44 -0300

    :construction: imported dependencies

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,9 @@
+import { split } from 'apollo-link'
 import { ApolloClient } from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws'
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({
   uri: 'https://api.graph.cool/simple/v1/cjjxgqfod261h0144fy4hky9h',
